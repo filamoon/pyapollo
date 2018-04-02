f8e7122a9f328d80b055562c59ec4bcec3b3a7e9
sunguangran <sgran8787@gmail.com>
2018-04-02 18:09:53 +0800

    some changes

--- pyapollo/apollo_client.py
+++ pyapollo/apollo_client.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
-import logging
+import socket
 import sys
 import threading
 import time
@@ -8,163 +8,134 @@ import time
 import requests
 
 
+def get_local_ip():
+    """
+    获取本地IP地址
+    :return:
+    """
+    s = None
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 53))
+        return s.getsockname()[0]
+    except Exception as e:
+        raise e
+    finally:
+        if s:
+            s.close()
+
+
 class ApolloClient(object):
-    def __init__(self, app_id, cluster='default', config_server_url='http://localhost:8080', timeout=35, ip=None):
-        self.config_server_url = config_server_url
+    """
+    apollo python client
+    使用说明: http://wiki.intra.yongqianbao.com/pages/viewpage.action?pageId=11928499
+    author: sunguangran@daixiaomi.com
+    """
+
+    def __init__(self, app_id, config_server, cluster='default', timeout=90, local_ip=None):
+        self.config_server = config_server
+        self._notification_map = {'application': -1}
+
         self.appId = app_id
         self.cluster = cluster
         self.timeout = timeout
         self.stopped = False
-        self.init_ip(ip)
-
         self._stopping = False
+        self.ip = local_ip if local_ip else get_local_ip()
+
+        # 本地缓存
         self._cache = {}
-        self._notification_map = {'application': -1}
 
-    def init_ip(self, ip):
-        if ip:
-            self.ip = ip
+    def _listener(self):
+        while not self._stopping:
+            self._long_poll()
+
+        self.stopped = True
+
+    # start the long polling loop
+    def start(self):
+        if len(self._cache) == 0:
+            self._long_poll()
+
+        t1 = threading.Thread(target=self._listener, args=())
+        t1.setDaemon(True)
+        t1.start()
+
+        return self
+
+    def stop(self):
+        self._stopping = True
+
+    def _http_get_cached(self, key, default_val, namespace='application'):
+        url = '{}/configfiles/json/{}/{}/{}?ip={}'.format(self.config_server, self.appId, self.cluster, namespace, self.ip)
+        resp = requests.get(url)
+        if resp.ok:
+            data = resp.json()
+            self._cache[namespace] = data
         else:
-            import socket
-            try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                s.connect(('8.8.8.8', 53))
-                ip = s.getsockname()[0]
-            finally:
-                s.close()
-            self.ip = ip
+            data = self._cache[namespace]
 
-    # Main method
-    def get_value(self, key, default_val=None, namespace='application', auto_fetch_on_cache_miss=False):
+        return data[key] if key in data else default_val
+
+    def _http_get_ignore_cache(self, namespace='application'):
+        url = '{}/configs/{}/{}/{}?ip={}'.format(self.config_server, self.appId, self.cluster, namespace, self.ip)
+        r = requests.get(url)
+        if r.status_code == 200:
+            data = r.json()
+            self._cache[namespace] = data['configurations']
+
+    def _long_poll(self):
+        url = '{}/notifications/v2'.format(self.config_server)
+        notifications = []
+        for key in self._notification_map:
+            notification_id = self._notification_map[key]
+            notifications.append({
+                'namespaceName' : key,
+                'notificationId': notification_id
+            })
+
+        resp = requests.get(url=url, params={
+            'appId'        : self.appId,
+            'cluster'      : self.cluster,
+            'notifications': json.dumps(notifications, ensure_ascii=False)
+        }, timeout=self.timeout)
+
+        if resp.status_code == 304:
+            return
+
+        if resp.status_code == 200:
+            data = resp.json()
+            for entry in data:
+                ns = entry['namespaceName']
+                nid = entry['notificationId']
+                self._http_get_ignore_cache(ns)
+                self._notification_map[ns] = nid
+        else:
+            time.sleep(self.timeout)
+
+    def get_value(self, key, default_val=None, namespace='application', auto_fetch=False):
         if namespace not in self._notification_map:
             self._notification_map[namespace] = -1
             self._long_poll()
-            logging.getLogger(__name__).info("Add namespace '%s' to local notification map", namespace)
 
         if namespace not in self._cache:
             self._cache[namespace] = {}
-            if auto_fetch_on_cache_miss:
-                return self._cached_http_get(key, default_val, namespace)
-            logging.getLogger(__name__).info("Add namespace '%s' to local cache", namespace)
-            # This is a new namespace, need to do a blocking fetch to populate the local cache
+            if auto_fetch:
+                return self._http_get_cached(key, default_val, namespace)
 
         if key in self._cache[namespace]:
             return self._cache[namespace][key]
 
         return default_val
 
-    # Start the long polling loop. Two modes are provided:
-    # 1: thread mode (default), create a worker thread to do the loop. Call self.stop() to quit the loop
-    # 2: eventlet mode (recommended), no need to call the .stop() since it is async
-    def start(self, use_eventlet=False):
-        # First do a blocking long poll to populate the local cache, otherwise we may get racing problems
-        if len(self._cache) == 0:
-            self._long_poll()
-        if use_eventlet:
-            import eventlet
-            eventlet.spawn(self._listener)
-        else:
-            import signal
-            signal.signal(signal.SIGINT, self._signal_handler)
-            signal.signal(signal.SIGTERM, self._signal_handler)
-            signal.signal(signal.SIGABRT, self._signal_handler)
-            t = threading.Thread(target=self._listener)
-            t.start()
-
-    def stop(self):
-        self._stopping = True
-        logging.getLogger(__name__).info("Stopping listener...")
-
-    def _cached_http_get(self, key, default_val, namespace='application'):
-        url = '{}/configfiles/json/{}/{}/{}?ip={}'.format(self.config_server_url, self.appId, self.cluster, namespace, self.ip)
-        r = requests.get(url)
-        if r.ok:
-            data = r.json()
-            self._cache[namespace] = data
-            logging.getLogger(__name__).info('Updated local cache for namespace %s', namespace)
-        else:
-            data = self._cache[namespace]
-
-        if key in data:
-            return data[key]
-        else:
-            return default_val
-
-    def _uncached_http_get(self, namespace='application'):
-        url = '{}/configs/{}/{}/{}?ip={}'.format(self.config_server_url, self.appId, self.cluster, namespace, self.ip)
-        r = requests.get(url)
-        if r.status_code == 200:
-            data = r.json()
-            self._cache[namespace] = data['configurations']
-            logging.getLogger(__name__).info('Updated local cache for namespace %s release key %s: %s',
-                                             namespace, data['releaseKey'],
-                                             repr(self._cache[namespace]))
-
-    def _signal_handler(self, signal, frame):
-        logging.getLogger(__name__).info('You pressed Ctrl+C!')
-        self._stopping = True
-
-    def _long_poll(self):
-        url = '{}/notifications/v2'.format(self.config_server_url)
-        notifications = []
-        for key in self._notification_map:
-            notification_id = self._notification_map[key]
-            notifications.append({
-                'namespaceName': key,
-                'notificationId': notification_id
-            })
-
-        r = requests.get(url=url, params={
-            'appId': self.appId,
-            'cluster': self.cluster,
-            'notifications': json.dumps(notifications, ensure_ascii=False)
-        }, timeout=self.timeout)
-
-        logging.getLogger(__name__).debug('Long polling returns %d: url=%s', r.status_code, r.request.url)
-
-        if r.status_code == 304:
-            # no change, loop
-            logging.getLogger(__name__).debug('No change, loop...')
-            return
-
-        if r.status_code == 200:
-            data = r.json()
-            for entry in data:
-                ns = entry['namespaceName']
-                nid = entry['notificationId']
-                logging.getLogger(__name__).info("%s has changes: notificationId=%d", ns, nid)
-                self._uncached_http_get(ns)
-                self._notification_map[ns] = nid
-        else:
-            logging.getLogger(__name__).warn('Sleep...')
-            time.sleep(self.timeout)
-
-    def _listener(self):
-        logging.getLogger(__name__).info('Entering listener loop...')
-        while not self._stopping:
-            self._long_poll()
-
-        logging.getLogger(__name__).info("Listener stopped!")
-        self.stopped = True
-
 
 if __name__ == '__main__':
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
-
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    root.addHandler(ch)
-
-    client = ApolloClient('pycrawler')
-    client.start()
+    client = ApolloClient(app_id=1001, config_server='http://192.168.1.255:11111', cluster='default', timeout=90).start()
     if sys.version_info[0] < 3:
         v = raw_input('Press any key to quit...')
     else:
         v = input('Press any key to quit...')
 
+    print(client.get_value(key='timeout', default_val='none', namespace='mue123'))
+
     client.stop()
-    while not client.stopped:
-        pass
